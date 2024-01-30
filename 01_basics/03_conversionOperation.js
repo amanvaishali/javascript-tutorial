e7707501f5ab8cf2529b1d5886674c8afbf6c114
amanvaishali <157941986+amanvaishali@users.noreply.github.com>
2024-01-30 19:07:02 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,15 @@
+// let score = "33";
+
+// console.log (typeof score);
+// console.log (typeof (score));
+
+let someNumber =33
+
+let stringNumber =String (someNumber);
+console.log (stringNumber);
+console.log (typeof someNumber);
+
+//"33" => 33//
+
+//"33abd" => NaN//
+//true => 1; false => 0//
