f0de0bc63e5599d52d000c84ecb07eb0d1d709cf
amanvaishali <157941986+amanvaishali@users.noreply.github.com>
2024-02-01 05:41:20 +0000

    dataTypess

---
+++ 01_basics/02_dataTypess.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version
+
+//alert( 4 + 5 ) // we are using nodejs, not browers
+
+console.log ( 3 
+    +
+     5 ) // code readability should be high
+
+     console.log ("aman")
+
+      //number =>  2 to power 65
+     // bigint
+    //  string => ""
+   //    boolean => true/false
+  //     null => standalone value
+  //     undefined =>
+  //     symbol => unique
+  //     object =>
