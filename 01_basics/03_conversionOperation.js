4cf526d3c8a702018c1cd82278c57223d6a38eec
amanvaishali <157941986+amanvaishali@users.noreply.github.com>
2024-02-02 12:32:35 +0000

    datatypessummer

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -3,13 +3,24 @@
 // console.log (typeof score);
 // console.log (typeof (score));
 
-let someNumber =33
+// let someNumber =33
 
-let stringNumber =String (someNumber);
-console.log (stringNumber);
-console.log (typeof someNumber);
+// let stringNumber =String (someNumber);
+// console.log (stringNumber);
+// console.log (typeof someNumber);
 
 //"33" => 33//
 
 //"33abd" => NaN//
 //true => 1; false => 0//
+
+//***********************Operation***********************
+let value = 3
+let negvalue = -value
+console.log (negvalue);
+
+let set1 = "hello"
+let set2 = "aman"
+let set3 = set1 + set2
+
+console.log(set3);
---
+++ 01_basics/datatypessummy.js
@@ -0,0 +1,30 @@
+// primitive
+
+// 7 types : string, number, null, Boolearn, underfined, Symbol, BigInt
+
+const score = 100
+const scorevalue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let undefined
+const Id = Symbol('123')
+const anotherId = Symbol ('123')
+console.log(Id ==== anotherId);
+const bigNumber = 423472364239842484864n
+
+
+//Reference (non Primitive)  
+
+// 3 types: Array, object, Function
+
+const array = ["aman", "rahul", "pavan"];
+letmyobj = {
+  Name:"aman",
+  age:22;
+
+}
+
+const myfunction = function (){
+    console.log("heelo world");
+    
+}
