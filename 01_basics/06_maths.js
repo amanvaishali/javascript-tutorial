5091ef26cf187c4d8d7f2602d814a27ee8b132bd
amanvaishali <157941986+amanvaishali@users.noreply.github.com>
2024-02-03 19:46:03 +0000

    maths

---
+++ 01_basics/06_maths.js
@@ -0,0 +1,8 @@
+let score = 500
+console.log(score);
+
+const blance = new Number(200)
+console.log(blance);
+console.log(blance.toString().length);
+console.log(blance.toFixed(2));
+console.log(typeof blance);
